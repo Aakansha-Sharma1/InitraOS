0a213610c124e1e1acc2c170b47b83a9cf5c6b1a
Aakansha-Sharma1 <aakansha.s0506@gmail.com>
2026-09-17 09:53:17 +0000

    Validate memory protection flags

--- tools/boottest.py
+++ tools/boottest.py
@@ -54,6 +54,8 @@ EXPECTED = [
     "[InitraOS] FRAME_VALIDATION_OK",
     "[InitraOS] FRAME_PAGING_OK",
     "[InitraOS] DYNAMIC_PAGE_OK",
+    "[InitraOS] PAGE_PROTECTION_OK",
+    "[InitraOS] PAGE_USER_PROTECTION_OK",
     "[InitraOS] HEAP_PAGING_OK",
     "[InitraOS] HEAP_DYNAMIC_OK",
     "[InitraOS] BEFORE_PG",
